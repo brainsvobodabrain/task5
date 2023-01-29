1f889c85f4c18a77a26c864c7707babb331f2af7
BraIN <brainsvoboda@mail.ru>
2023-01-29 21:08:36 +0300

    complete task2

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+int [] create_random_array(int count){
+    int [] ret =new int[count];
+    Random rand=new Random();
+    if (count>0){
+        for (int i=0; i<count; i++){
+            ret[i]=rand.Next(0,100);
+        }        
+    }
+    return ret;
+}
+
+string print_array(int[] array){
+    return ("["+string.Join(", ",array)+"]");
+}
+
+int summ_oddpos_elements(int[] arr){
+    int summ=0;
+    for (int i=0; i<arr.Length; i++){
+        if (i%2!=0){
+            summ=summ+arr[i];
+        }
+    }
+    return summ;
+}
+int[] array=create_random_array(4);
+System.Console.WriteLine($"{print_array(array)} -> {summ_oddpos_elements(array)}");
