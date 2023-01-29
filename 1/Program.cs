2345ef544f5d35f20e8f8a3af9b99c77557d6af9
BraIN <brainsvoboda@mail.ru>
2023-01-29 20:58:02 +0300

    task1 comlete

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+int [] create_random_array(int count){
+    int [] ret =new int[count];
+    Random rand=new Random();
+    if (count>0){
+        for (int i=0; i<count; i++){
+            ret[i]=rand.Next(100,1000);
+        }        
+    }
+    return ret;
+}
+int count_honest(int[] array){
+    int ret=0;
+    for (int i=0; i<array.Length;i++){
+        if (array[i]%2==0){
+            ret++;
+        }
+    }
+    return ret;
+}
+string print_array(int[] array){
+    return ("["+string.Join(", ",array)+"]");
+}
+
+int[] array=create_random_array(10);
+System.Console.WriteLine($"Колличество четных чисел в массиве {print_array(array)} составляет {count_honest(array)}");
